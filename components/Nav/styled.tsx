c62d252d04e08939628002a60f117b1de9300076
hecsanchez <hi@hecsanchez.com>
2024-02-10 21:43:59 -0600

    💄 update Nav style

--- components/Nav/styled.tsx
+++ components/Nav/styled.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const NavWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  gap: 10px;
+  gap: 20px;
 `
 
 export default NavWrapper;
--- components/NavItem/index.tsx
+++ components/NavItem/index.tsx
@@ -8,14 +8,25 @@ interface NavItemWrapperProps {
 const NavItemWrapper = styled(Link)`
   display: flex;
   justify-content: center;
-  padding: 5px;
-  border-bottom: 2px solid;
-  border-color: ${(props: NavItemWrapperProps)=> props.$active ? 'black' : 'transparent'};
+  padding: 5px 0;
   text-decoration: none;
   color: ${(props: NavItemWrapperProps)=> props.$active ? '#323232' : '#b4b4b4'};
   font-weight: 500;
   font-size: 18px;
   transition: color .2s ease;
+  position: relative;
+  
+  &:after {
+    ${(props) => props.$active && `
+        content: '';
+        bottom: -17px;
+        border-bottom: 4px solid;
+        border-color: black;
+        width: 100%;
+        display: block;
+        position: absolute;
+    `}
+  }
   
   &:hover {
     color: #323232;
