1dfd7c2bfc3a5d3239b7b88b42da9dfc7a132e60
hecsanchez <hi@hecsanchez.com>
2024-02-12 10:46:41 -0600

    🐛 fix gap on nav

--- components/Nav/styled.tsx
+++ components/Nav/styled.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const NavWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  
-  @media scren and (min-width: 767px) {
+
+  @media screen and (min-width: 767px) {
     gap: 20px;
   }
 `;
